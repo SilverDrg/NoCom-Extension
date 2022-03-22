461b4683f0952eeb376a81f5f11be454e0269c6b
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-03-22 21:09:52 +0100

    added file for individual comments

---
+++ src/components/comment-components/Comment.tsx
@@ -0,0 +1,22 @@
+import { Grid, Paper, Typography } from '@mui/material'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import React from 'react'
+
+const Comment = () => {
+  return (
+    <Paper>
+        <Typography>
+            Username
+        </Typography>
+        <Typography>
+            Random comment provided by the user.
+        </Typography>
+        <Grid>
+
+        </Grid>
+    </Paper>
+  )
+}
+
+export default Comment
--- src/components/comment-components/Comments.tsx
+++ src/components/comment-components/Comments.tsx
@@ -1,8 +1,47 @@
+import { Box, Container, CssBaseline } from '@mui/material'
 import React from 'react'
+
+const userComments = [
+  {
+    comment: "Neki komentar za testiranje",
+    user: "Anonymous",
+    likes: "1",
+    nsfw: "false"
+  },
+  {
+    comment: "Very nice comment owo",
+    user: "Silver",
+    likes: "1",
+    nsfw: "false"
+  },
+  {
+    comment: "Neki komentar za testiranje",
+    user: "User123",
+    likes: "1",
+    nsfw: "false"
+  },
+  {
+    comment: "A random comment for testing purposes",
+    user: "Anonymous",
+    likes: "1",
+    nsfw: "false"
+  }
+]
 
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline/>
+      <Box
+        sx={{
+        marginTop: 2,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        }}
+      >
+      </Box>
+    </Container>
   )
 }
 

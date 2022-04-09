48eb0a83447b478619e36a1185a679eae7914fe3
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-04-09 21:38:20 +0200

    Updated Comment and CommentNew, as well as added event handling

--- src/components/comment-components/Comment.tsx
+++ src/components/comment-components/Comment.tsx
@@ -1,19 +1,27 @@
-import { Grid, Paper, Typography } from '@mui/material'
+import { Grid, Paper, Typography, Checkbox } from '@mui/material'
+import { pink } from '@mui/material/colors';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import React from 'react'
+import { useState } from 'react'
 
 const Comment = (props: any) => {
+  const [Like, setLike] = useState(false);
   const user = props.user;
   let isNSFW;
+
+  const handleLike = () => {
+    setLike(!Like);
+  }
+
   if (user.nsfw === "true") {
     isNSFW = 
     <Grid item xs={3} md={3}>
       <Typography variant="button" color="red">NSFW</Typography>
     </Grid>;
   }
+
   return (
-    <Paper elevation={3} sx={{ m: 1, width: '90%'}}>
+    <Paper elevation={3} sx={{ m: 1, width: '96%'}}>
         <Typography variant="body1" align="left" sx={{ m: 1, borderBottom: 1, borderColor: 'primary.main'}}>
             {user.username}
         </Typography>
@@ -22,11 +30,24 @@ const Comment = (props: any) => {
         </Typography>
         <Grid container>
           <Grid item container spacing={0} xs={9} md={9}>
-            <Grid item xs={3} md={3}>
-              <FavoriteBorderIcon/>
+            <Grid item xs={1} md={2} sx={{ ml: 1 }}>
+              <Checkbox 
+                icon={<FavoriteBorderIcon />} 
+                checkedIcon={<FavoriteIcon />} 
+                value="nsfw" 
+                checked={Like} 
+                onChange={handleLike} 
+                size="small" 
+                sx={{
+                  color: 'primary',
+                  '&.Mui-checked': {
+                    color: pink[600],
+                  },
+                }}
+              />
             </Grid>
             <Grid item xs={3} md={0.5}>
-              {user.likes}
+              <Typography variant="body1" align="left" sx={{ p: 1, ml: 1 }} color={Like ? pink[500] : "" } >{user.likes}</Typography>
             </Grid>
           </Grid>
           {isNSFW}
--- src/components/comment-components/CommentNew.tsx
+++ src/components/comment-components/CommentNew.tsx
@@ -1,75 +1,91 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import { useState } from 'react';
+import { Avatar, Button, TextField, Grid, Box, Typography, Container, FormControlLabel, Checkbox } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { Link } from 'react-router-dom';
 
 const CommentNew = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('comment'),
-    });
-  };
+    const [Anonymous, setAnonymous] = useState(false);
+    const [Nsfw, setNsfw] = useState(false);
 
-  return (
-    <Container component="main" maxWidth="xs">
-    <Box
-        sx={{
-        marginTop: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        }}
-    >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.dark' }}>
-            <ChatIcon />
-        </Avatar>
-        <Typography component="h1" variant="h6">
-            Post a new comment
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ width: '96%', mt: 1 }}>
-            <TextField
-                variant="filled"
-                margin="dense"
-                size="small"
-                multiline
-                required
-                fullWidth
-                rows={4}
-                id="comment"
-                label="New comment"
-                name="comment"
-                autoComplete="comment"
-                autoFocus
-            />
-            <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-            >
-                Post
-            </Button>
-            <Grid container>
-                <Grid item>
-                    <Link to="/sign-in" style={{ textDecoration: 'none' }}>
-                        <Typography color="primary.dark" variant="body2">
-                            {"Back to Sign in"}
-                        </Typography>
-                    </Link>
+    const handleAnonymous = () => {
+        setAnonymous(!Anonymous);
+    }
+
+    const handleNsfw = () => {
+        setNsfw(!Nsfw);
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+        content: data.get('comment'),
+        anonymous: Anonymous,
+        nsfw: Nsfw
+        });
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+        <Box
+            sx={{
+            marginTop: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            }}
+        >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.dark' }}>
+                <ChatIcon />
+            </Avatar>
+            <Typography component="h1" variant="h6">
+                Post a new comment
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ width: '96%', mt: 1 }}>
+                <TextField
+                    variant="filled"
+                    margin="dense"
+                    size="small"
+                    multiline
+                    required
+                    fullWidth
+                    rows={4}
+                    id="comment"
+                    label="New comment"
+                    name="comment"
+                    autoComplete="comment"
+                    autoFocus
+                />
+                <FormControlLabel
+                    control={<Checkbox value="anonymous" checked={Anonymous} onChange={handleAnonymous} color="primary" />}
+                    label="Anonymous"
+                />
+                <FormControlLabel
+                    control={<Checkbox value="nsfw" checked={Nsfw} onChange={handleNsfw} color="primary" />}
+                    label="Nsfw"
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                    Post
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <Link to="/comments" style={{ textDecoration: 'none' }}>
+                            <Typography color="primary.dark" variant="body2">
+                                {"Back to comments"}
+                            </Typography>
+                        </Link>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Box>
         </Box>
-    </Box>
-    </Container>
-  );
+        </Container>
+    );
 }
 
 export default CommentNew;

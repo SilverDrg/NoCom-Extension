43b9996b0c135def89a03d8c1bf43f33f94b6107
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-05-19 00:19:49 +0200

    Created profile page

--- src/components/user-components/Profile.tsx
+++ src/components/user-components/Profile.tsx
@@ -1,28 +1,85 @@
-import { Container, Box, Paper, Typography, Grid, Card, CardHeader, CardMedia, CardContent } from '@mui/material'
+import { Container, Box, Typography, Grid, Avatar, Tabs, Tab, Fab } from '@mui/material'
 import React from 'react'
+import { Link } from 'react-router-dom';
 import Placeholder from '../../images/DogPlaceholder.jpg';
+import BannerPlaceholder from '../../images/wolf.jpg';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 const Profile = () => {
-  return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-        marginTop: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        }}
-      >
-        <Grid container>
-            <Grid item container>
+  const [tab, setTab] = React.useState(0);
 
-            </Grid>
-            <Grid item>
-                
-            </Grid>
+  const ChangeTab = (event: React.SyntheticEvent, newTab: number) => {
+    setTab(newTab);
+  };
+  return (
+    <Box
+      sx={{
+      marginTop: 0.5,
+      padding: 0,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      }}
+    >
+      <Grid container sx={{ p: 0, m: 0 }}>
+        <Grid item container>
+          <Fab color="secondary" size="medium" component={Link} to={'/settings'} sx={{ position: 'absolute', right: 8, top: 72 }}>
+            <SettingsIcon />
+          </Fab>
+          <Grid item xs={12} md={6}>
+            <Box sx={{ width: '100%', height: 128 }}>
+              <Box component="img" alt="Banner" src={BannerPlaceholder} sx={{ width: '100%', height: 128 , objectFit: 'cover'}} className=""/>
+            </Box>
+            <Avatar alt="Dog" src={ Placeholder } sx={{ width: 92, height: 92, ml: 2, mt: -4, border: 2, borderColor: 'white' }} />
+          </Grid>
+          <Grid item xs={2} md={4}>
+            <Typography variant="h6">Dog</Typography>
+          </Grid>
         </Grid>
-      </Box>
-    </Container>
+        <Grid item xs={12}>
+            <Tabs value={tab} onChange={ChangeTab} centered>
+              <Tab label="Comments" sx={{ width: '33%'}}/>
+              <Tab label="Likes" sx={{ width: '33%'}}/>
+              <Tab label="Top" sx={{ width: '33%'}}/>
+            </Tabs>
+            <TabPanel value={tab} index={0}>
+              First panel
+            </TabPanel>
+            <TabPanel value={tab} index={1}>
+              Second panel
+            </TabPanel>
+            <TabPanel value={tab} index={2}>
+              Third panel
+            </TabPanel>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 

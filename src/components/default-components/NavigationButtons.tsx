b02270a19d9aa8619e73e677ce3c45687b6a30c3
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-05-19 18:52:42 +0200

    Added theme switch to navigation

--- src/components/default-components/NavigationButtons.tsx
+++ src/components/default-components/NavigationButtons.tsx
@@ -1,14 +1,23 @@
 import { useContext } from 'react';
 import { Button, IconButton, Box } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { TokenContext } from '../session-components/TokenContextProvider';
+import { ColorModeContext } from '../session-components/ThemeContextProvider';
+
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Link } from 'react-router-dom';
-import { TokenContext } from '../session-components/TokenContextProvider';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 const NavigationButtons = () => {
   const {token, setToken} = useContext(TokenContext);
+  const {mode, setMode} = useContext(ColorModeContext);
   const SignedIn = token !== null;
+
+  const onClickToggleMode = () => {
+    mode === 'light' ? setMode('dark') : setMode('light')
+  }
   
   if (SignedIn) {
     return (
@@ -25,6 +34,9 @@ const NavigationButtons = () => {
 
   return (
     <Box sx={{ ml: "auto" }}>
+      <IconButton sx={{ ml: 1, mr: 1, width: 42, minWidth: 36}} color="primary" onClick={onClickToggleMode}>
+        {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
       <Button sx={{ width: 42, minWidth: 36}} variant="contained" component={Link} to={`/sign-in`} color="secondary">
           <LoginIcon color="primary"/>
       </Button>
--- src/components/user-components/Settings.tsx
+++ src/components/user-components/Settings.tsx
@@ -1,11 +1,11 @@
 import { Box, Typography, Grid, Avatar, Fab } from '@mui/material'
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { ColorModeContext } from '../session-components/ThemeContextProvider';
+
 import Placeholder from '../../images/DogPlaceholder.jpg';
 import BannerPlaceholder from '../../images/wolf.jpg';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { ColorModeContext } from '../session-components/ThemeContextProvider';
 
 const Settings = () => {
     const {mode, setMode} = useContext(ColorModeContext);
@@ -17,27 +17,27 @@ const Settings = () => {
     return (
         <Box
         sx={{
-        marginTop: 0,
-        padding: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+            marginTop: 0,
+            padding: 0,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
         }}
         >
         <Grid container sx={{ p: 0, m: 0 }}>
             <Grid item container>
-            <Fab color="secondary" size="medium" component={Link} to={'/settings'} sx={{ position: 'absolute', right: 8, top: 72 }} onClick={onClickToggleMode}>
+            <Fab color="secondary" size="medium" sx={{ position: 'absolute', right: 8, top: 72 }} onClick={onClickToggleMode}>
                 {mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
             </Fab>
             <Grid item xs={12} md={12}>
                 <Box sx={{ width: '100%', height: 128 }}>
-                <Box component="img" alt="Banner" src={BannerPlaceholder} sx={{ width: '100%', height: 128 , objectFit: 'cover'}} className=""/>
+                    <Box component="img" alt="Banner" src={BannerPlaceholder} sx={{ width: '100%', height: 128 , objectFit: 'cover'}} className=""/>
                 </Box>
                 <Avatar alt="Dog" src={ Placeholder } sx={{ width: 92, height: 92, ml: 2, mt: -4, border: 2, borderColor: 'background.default' }} />
             </Grid>
             <Grid item xs={12} md={12}>
                 <Typography variant="h6" align="left" sx={{ ml: 2, mb: 1, mt: 1 }}>
-                Dog
+                    Dog
                 </Typography>
             </Grid>
             </Grid>

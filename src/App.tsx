44c23d631da4353e333dd7c0802e311df1b32eb0
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2022-11-01 20:24:59 +0100

    Updated imports for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { CssBaseline, Box } from '@mui/material';
 import TokenContextProvider from './components/session/TokenContextProvider';
-import { Title, Navigation, Home, Settings } from '@mui/icons-material';
+import ThemeContextProvider from './components/session/ThemeContextProvider';
+import { Title } from './components/general/Title';
+import { Home } from './components/general/Home';
+import { AboutUs } from './components/general/AboutUs';
+import { Navigation } from './components/general/Navigation';
+import { Footer } from './components/general/Footer';
+
 import { CommentNew } from './components/comment/CommentNew';
 import { CommentNewButton } from './components/comment/CommentNewButton';
 import { Comments } from './components/comment/Comments';
-import { AboutUs } from './components/general/AboutUs';
-import { Footer } from './components/general/Footer';
-import ThemeContextProvider from './components/session/ThemeContextProvider';
+
 import { ForgotPassword } from './components/user/ForgotPassword';
 import { Profile } from './components/user/Profile';
+import { Settings } from './components/user/Settings';
 import { SignIn } from './components/user/SignIn';
 import { SignUp } from './components/user/SignUp';
 
 import './App.css';
-
-declare module '@mui/material/styles' {
-  interface Theme {
-    status: {
-      danger: string;
-    };
-  }
-  // allow configuration using `createTheme`
-  interface ThemeOptions {
-    status?: {
-      danger?: string;
-    };
-  }
-}
 
 const App: React.FC = () => {
   return (

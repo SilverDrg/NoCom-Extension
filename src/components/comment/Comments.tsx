ff96b4667e58e318fb18d7bf49af90fc27d32c36
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-03 15:49:19 +0100

    Comments are fetched from api

--- src/components/comment/Comments.tsx
+++ src/components/comment/Comments.tsx
@@ -1,64 +1,57 @@
-import { Box, Container } from '@mui/material'
-import { Comment } from './Comment';
-
-const userComments = [
-  {
-    id: 1,
-    comment: "Neki komentar za testiranje",
-    username: "Anonymous",
-    likes: "4",
-    nsfw: "false"
-  },
-  {
-    id: 2,
-    comment: "Very nice comment owo",
-    username: "Silver",
-    likes: "2",
-    nsfw: "true"
-  },
-  {
-    id: 3,
-    comment: "Neki komentar za testiranje",
-    username: "User123",
-    likes: "3",
-    nsfw: "true"
-  },
-  {
-    id: 4,
-    comment: "A random comment for testing purposes",
-    username: "Anonymous",
-    likes: "1",
-    nsfw: "false"
-  },
-  {
-    id: 5,
-    comment: "A random comment for testing purposes",
-    username: "Anonymous",
-    likes: "11",
-    nsfw: "false"
-  },
-  {
-    id: 6,
-    comment: "A random comment for testing purposes",
-    username: "Anonymous",
-    likes: "111111",
-    nsfw: "true"
-  }
-]
+import React from 'react';
+import { Box, Container, Button } from "@mui/material";
+import { useCommentsList } from "../../hooks/useCommentsList";
+import { Comment } from "./Comment";
 
 export const Comments = () => {
+  const [InfiniteLoad, setInfiniteLoad] = React.useState(false);
+  const [comments, loadMoreComments] = useCommentsList();
+
+  const LoadMore = React.useCallback(() => {
+    loadMoreComments();
+    setInfiniteLoad(true);
+  }, [loadMoreComments])
+
+  React.useEffect(() => {
+    const ScrollLoad = (event: any) => {
+      const bottom = event.target.scrollingElement.clientHeight + event.target.scrollingElement.scrollTop >= event.target.scrollingElement.scrollHeight - 5;
+      if(bottom && InfiniteLoad){
+        loadMoreComments();
+      }
+    };
+
+    window.addEventListener('scroll', ScrollLoad);
+
+    return () => {
+      window.removeEventListener('scroll', ScrollLoad);
+    };
+  }, [InfiniteLoad, loadMoreComments]);
+
   return (
-    <Container component="main" maxWidth="xs" sx={{ pl: 1, pr: 1}}>
+    <Container component="main" maxWidth="xs" sx={{ pl: 1, pr: 1 }}>
       <Box
         sx={{
-        marginTop: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+          marginTop: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        {userComments.map((user) => (<Comment key={user.id} user={user}/>))}
+        {comments &&
+          comments.map((comment) => (
+            <Comment key={comment.id} comment={comment} />
+          ))}
+      </Box>
+      <Box
+        sx={{
+          marginTop: 2,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Button variant="contained" onClick={LoadMore} color="secondary">Load more</Button>
       </Box>
     </Container>
-  )
-}
+  );
+};

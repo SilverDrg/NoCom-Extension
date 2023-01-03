92d3aba91473dd781f91964c2a57900abad2967f
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-03 15:48:59 +0100

    Updated app theme

--- src/components/general/Navigation.tsx
+++ src/components/general/Navigation.tsx
@@ -1,6 +1,6 @@
 import Logo from '../../images/Logo-white.png';
 import React from 'react';
-import { AppBar, Toolbar, Tabs, Tab } from '@mui/material';
+import { AppBar, Toolbar, Tabs, Tab, useTheme } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import ChatIcon from '@mui/icons-material/Chat';
 import { Link } from 'react-router-dom';
@@ -9,13 +9,14 @@ import { NavigationButtons } from './NavigationButtons';
 
 export const Navigation = () => {
   const [value, setValue] = React.useState(0);
+  const theme = useTheme();
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
   
   return (
-    <AppBar position="sticky" sx={{ background: "#94EEFF" }}>
+    <AppBar position="sticky" sx={{ background: theme.palette.primary.main }}>
       <Toolbar>
         <Link to={'/home'}>
           <img src={Logo} alt="NoCom" width="80px" />
@@ -24,10 +25,11 @@ export const Navigation = () => {
           value={value}
           onChange={handleChange}
           sx={{ marginleft: "auto", /*margin: "auto"*/ }} 
-          textColor="primary" 
+          textColor="secondary"
+          indicatorColor="secondary" 
         >
-          <Tab icon={<HomeIcon color="secondary" fontSize='large' />} sx={{ width: 48, minWidth: 36 }} component={Link} to={`/home`}/>
-          <Tab icon={<ChatIcon color="secondary" fontSize='large' />} sx={{ width: 48, minWidth: 36 }} component={Link} to={`/comments`}/>
+          <Tab icon={<HomeIcon color="secondary" fontSize='large' />} aria-label="home" sx={{ width: 48, minWidth: 36 }} component={Link} to={`/home`}/>
+          <Tab icon={<ChatIcon color="secondary" fontSize='large' />} aria-label="comments" sx={{ width: 48, minWidth: 36 }} component={Link} to={`/comments`}/>
         </Tabs>
         <NavigationButtons />
       </Toolbar>
--- src/components/session/ThemeContextProvider.tsx
+++ src/components/session/ThemeContextProvider.tsx
@@ -1,64 +1,67 @@
-import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material';
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material";
 
-export const ColorModeContext = React.createContext({ 
-    mode: 'light',
-    setMode: (mode: 'light' | 'dark') => {}
+export const ColorModeContext = React.createContext({
+  mode: "light",
+  setMode: (mode: "light" | "dark") => {},
 });
 
 const ColorModeContextProvider: React.FC = ({ children }) => {
-    const [mode, setMode] = React.useState<'light' | 'dark'>(() => {
-        if(localStorage.getItem('theme') === null || localStorage.getItem('theme') === 'light') {
-            return 'light';
-        } else if (localStorage.getItem('theme') === 'dark') {
-            return 'dark';
-        } else {
-            return 'light';
-        }
-    });
+  const [mode, setMode] = React.useState<"light" | "dark">(() => {
+    if (
+      localStorage.getItem("theme") === null ||
+      localStorage.getItem("theme") === "light"
+    ) {
+      return "light";
+    } else if (localStorage.getItem("theme") === "dark") {
+      return "dark";
+    } else {
+      return "light";
+    }
+  });
 
-    const contextValue = React.useMemo(() => ({mode, setMode}),[mode])
+  const contextValue = React.useMemo(() => ({ mode, setMode }), [mode]);
 
-    const value = React.useMemo(() => createTheme({
+  const value = React.useMemo(
+    () =>
+      createTheme({
         palette: {
-        mode,
-            ...(mode === 'dark' ? {
-            primary: {
-                light: '#c8ffff',
-                main: '#94eeff',
-                dark: '#60bbcc',
-                contrastText: '#000000',
-            },
-            secondary: {
-                light: '#6b6f77',
-                main: '#40444b',
-                dark: '#202225',
-                contrastText: '#ffffff',
-            },
-            background: {
-                default: '#2f3136',
-                paper: '#2f3136'
-            }
-        } : {
-            primary: {
-            light: '#c8ffff',
-            main: '#94eeff',
-            dark: '#60bbcc',
-            contrastText: '#000000',
-            },
-            secondary: {
-            light: '#ffffff',
-            main: '#efefef',
-            dark: '#bdbdbd',
-            contrastText: '#000000',
-            },
-        }),
+          mode,
+          ...(mode === "dark"
+            ? {
+                primary: {
+                  main: "#2f343d",
+                  contrastText: "#000000",
+                },
+                secondary: {
+                  main: "#bac0ce",
+                  contrastText: "#161c26",
+                },
+                background: {
+                  default: "#161c26",
+                  paper: "#2f343d",
+                },
+              }
+            : {
+                primary: {
+                  main: "#0071EB",
+                  contrastText: "#000000",
+                },
+                secondary: {
+                  main: "#efefef",
+                  contrastText: "#000000",
+                },
+              }),
         },
-    }),
+      }),
     [mode]
-    );
+  );
 
-    return <ColorModeContext.Provider value={contextValue}><ThemeProvider theme={value}>{children}</ThemeProvider></ColorModeContext.Provider>
-}
+  return (
+    <ColorModeContext.Provider value={contextValue}>
+      <ThemeProvider theme={value}>{children}</ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+};
 
 export default ColorModeContextProvider;

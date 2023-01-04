150a91324a51d0a66b3ed5c37d80f6da0037f6fd
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-04 18:01:23 +0100

    Added viewing replies Updated grid layout Updated color based on theme

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,59 +1,113 @@
-import { Grid, Paper, Typography, Checkbox } from '@mui/material'
-import { pink } from '@mui/material/colors';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { useState } from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Grid, Paper, Typography, Checkbox, useTheme } from "@mui/material";
+import { pink } from "@mui/material/colors";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 
-import { CommentModel } from '../../models/Comment';
+import { ColorModeContext } from '../session/ThemeContextProvider';
+import { CommentModel } from "../../models/Comment";
 
 export const Comment = (props: any) => {
-  const [Like, setLike] = useState(false);
   const comment: CommentModel = props.comment;
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const { mode } = React.useContext(ColorModeContext);
+  const [Like, setLike] = React.useState(false);
   let isNSFW;
 
   const handleLike = () => {
     setLike((like) => !like);
+  };
+
+  const handleViewReplies = () => {
+    navigate(`/comments/${comment.id}`);
   }
 
+  console.log(comment.createdAt);
+
   if (comment.nsfw) {
-    isNSFW = 
-    <Grid item xs={3} md={3}>
-      <Typography variant="button" color="red">NSFW</Typography>
-    </Grid>;
+    isNSFW = (
+      <Grid item xs={3} md={3}>
+        <Typography variant='button' color='red'>
+          NSFW
+        </Typography>
+      </Grid>
+    );
   }
 
   return (
-    <Paper elevation={3} sx={{ width: '100%', m: 1, mt: 0.5, mb: 1 }}>
-        <Typography variant="body1" align="left" sx={{ m: 1, borderBottom: 1, borderColor: 'primary.main'}}>
-            {comment.username}
-        </Typography>
-        <Typography variant="body2" align="left" sx={{ m: 1}}>
-            {comment.content}
-        </Typography>
-        <Grid container>
-          <Grid item container spacing={0} xs={9} md={9}>
-            <Grid item xs={1} md={2} sx={{ ml: 1 }}>
-              <Checkbox 
-                icon={<FavoriteBorderIcon />} 
-                checkedIcon={<FavoriteIcon />} 
-                value="nsfw" 
-                checked={Like} 
-                onChange={handleLike} 
-                size="small" 
+    <Paper
+      key={comment.id}
+      elevation={3}
+      sx={{ m: 1, mt: 0.5, mb: 1, p: 0.5 }}
+    >
+      <Typography
+        variant='body1'
+        align='left'
+        sx={{ m: 1, borderBottom: 1, borderColor: mode === 'light' ? theme.palette.primary.main : theme.palette.secondary.light }}
+      >
+        {comment.username}
+      </Typography>
+      <Typography variant='body2' align='left' sx={{ m: 1 }}>
+        {comment.content}
+      </Typography>
+      <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justifyContent='start' spacing={2}>
+            <Grid item>
+              <Checkbox
+                icon={<ChatBubbleOutlineIcon />}
+                checkedIcon={<ChatBubbleOutlineIcon />}
+                value='replies'
+                checked={Like}
+                onChange={handleViewReplies}
                 sx={{
-                  color: 'primary',
-                  '&.Mui-checked': {
+                  color: "primary",
+                  "&.Mui-checked": {
                     color: pink[600],
                   },
+                  "& .MuiSvgIcon-root": { fontSize: 18 },
                 }}
               />
+              <Typography
+                variant='body2'
+                align='left'
+                sx={{ p: 1, pl: 0, display: "inline-block" }}
+                color={Like ? pink[500] : ""}
+              >
+                {comment.likes}
+              </Typography>
             </Grid>
-            <Grid item xs={3} md={0.5}>
-              <Typography variant="body1" align="left" sx={{ p: 1, ml: 1 }} color={Like ? pink[500] : "" } >{comment.likes}</Typography>
+            <Grid item>
+              <Checkbox
+                icon={<FavoriteBorderIcon />}
+                checkedIcon={<FavoriteIcon />}
+                value='like'
+                checked={Like}
+                onChange={handleLike}
+                sx={{
+                  color: "primary",
+                  "&.Mui-checked": {
+                    color: pink[600],
+                  },
+                  "& .MuiSvgIcon-root": { fontSize: 18 },
+                }}
+              />
+              <Typography
+                variant='body2'
+                align='left'
+                sx={{ p: 1, pl: 0, display: "inline-block" }}
+                color={Like ? pink[500] : ""}
+              >
+                {comment.likes}
+              </Typography>
             </Grid>
           </Grid>
           {isNSFW}
         </Grid>
+      </Grid>
     </Paper>
-  )
-}
+  );
+};

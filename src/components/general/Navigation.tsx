e601ad4142e0ce1cf77b698f8061177e90013920
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-04 17:36:06 +0100

    Updated navigation bar layout Removed logo

--- src/components/general/Navigation.tsx
+++ src/components/general/Navigation.tsx
@@ -1,15 +1,47 @@
-import Logo from '../../images/Logo-white.png';
 import React from 'react';
-import { AppBar, Toolbar, Tabs, Tab, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, Tabs, Tab, IconButton, useTheme } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 import ChatIcon from '@mui/icons-material/Chat';
-import { Link } from 'react-router-dom';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
+import { TokenContext } from '../session/TokenContextProvider';
+import { ColorModeContext } from '../session/ThemeContextProvider';
 import { NavigationButtons } from './NavigationButtons';
 
 export const Navigation = () => {
-  const [value, setValue] = React.useState(0);
+  const {token, setToken} = React.useContext(TokenContext);
+  const {mode, setMode} = React.useContext(ColorModeContext);
+  const [value, setValue] = React.useState(1);
+  const [expired, setExpired] = React.useState<boolean>();
   const theme = useTheme();
+
+  React.useEffect(() => {
+    if(localStorage.getItem('expiration') !== null) {
+      var expires = new Date(localStorage.getItem('expiration')!);
+      var now = new Date();
+      if(expires === null || expires < now) {
+        setExpired(true)
+        setToken(null)
+      } else {
+        setExpired(false)
+      }
+    }
+  }, [setToken]);
+
+  const SignedIn = token !== null;
+
+  const onClickToggleMode = () => {
+    if(mode === 'light') { 
+      setMode('dark')
+      localStorage.setItem('theme', 'dark');
+    } else { 
+      setMode('light')
+      localStorage.setItem('theme', 'light');
+    }
+  }
 
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
@@ -18,19 +50,20 @@ export const Navigation = () => {
   return (
     <AppBar position="sticky" sx={{ background: theme.palette.primary.main }}>
       <Toolbar>
-        <Link to={'/home'}>
-          <img src={Logo} alt="NoCom" width="80px" />
-        </Link>
         <Tabs
           value={value}
           onChange={handleChange}
-          sx={{ marginleft: "auto", /*margin: "auto"*/ }} 
+          sx={{ marginleft: "auto" }} 
           textColor="secondary"
           indicatorColor="secondary" 
         >
-          <Tab icon={<HomeIcon color="secondary" fontSize='large' />} aria-label="home" sx={{ width: 48, minWidth: 36 }} component={Link} to={`/home`}/>
-          <Tab icon={<ChatIcon color="secondary" fontSize='large' />} aria-label="comments" sx={{ width: 48, minWidth: 36 }} component={Link} to={`/comments`}/>
+          <Tab icon={<HomeIcon color="secondary" fontSize='large' />} aria-label="home" sx={{ minWidth: 36 }} component={Link} to={`/home`}/>
+          <Tab icon={<ChatIcon color="secondary" fontSize='large' />} aria-label="comments" sx={{ minWidth: 36 }} component={Link} to={`/comments`}/>
+          {SignedIn && !expired && <Tab icon={<AccountCircleIcon color="secondary" fontSize="large"/>} aria-label="comments" sx={{ minWidth: 36 }} component={Link} to={`/profile`}/>}
         </Tabs>
+        <IconButton sx={{ ml: 1, mr: 1, width: 42, minWidth: 36}} color="secondary" onClick={onClickToggleMode}>
+          {mode === 'dark' ? <DarkModeIcon fontSize="large" /> : <LightModeIcon fontSize="large" />}
+        </IconButton>
         <NavigationButtons />
       </Toolbar>
     </AppBar>
--- src/components/general/NavigationButtons.tsx
+++ src/components/general/NavigationButtons.tsx
@@ -1,22 +1,17 @@
-import { useContext, useState, useEffect } from 'react';
-import { Button, IconButton, Box } from '@mui/material';
+import React from 'react';
+import { Button, Box } from '@mui/material';
 import { Link, useNavigate } from 'react-router-dom';
 import { TokenContext } from '../session/TokenContextProvider';
-import { ColorModeContext } from '../session/ThemeContextProvider';
 
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import LightModeIcon from '@mui/icons-material/LightMode';
 
 export const NavigationButtons = () => {
-  const {token, setToken} = useContext(TokenContext);
-  const {mode, setMode} = useContext(ColorModeContext);
-  const [expired, setExpired] = useState<boolean>();
+  const {token, setToken} = React.useContext(TokenContext);
+  const [expired, setExpired] = React.useState<boolean>();
   const navigate = useNavigate();
 
-  useEffect(() => {
+  React.useEffect(() => {
     if(localStorage.getItem('expiration') !== null) {
       var expires = new Date(localStorage.getItem('expiration')!);
       var now = new Date();
@@ -31,16 +26,6 @@ export const NavigationButtons = () => {
 
   const SignedIn = token !== null;
 
-  const onClickToggleMode = () => {
-    if(mode === 'light') { 
-      setMode('dark')
-      localStorage.setItem('theme', 'dark');
-    } else { 
-      setMode('light')
-      localStorage.setItem('theme', 'light');
-    }
-  }
-
   const onClickSignOut = () => {
     setToken(null)
     navigate('/comments')
@@ -49,9 +34,6 @@ export const NavigationButtons = () => {
   if (SignedIn && !expired) {
     return (
       <Box sx={{ ml: "auto" }}>
-        <IconButton sx={{ ml: 1, mr: 1, width: 42, minWidth: 36}} component={Link} to={`/profile`} color="primary">
-            <AccountCircleIcon color="secondary" fontSize="large"/>
-        </IconButton>
         <Button sx={{ m: 1, width: 42, minWidth: 36}} variant="contained" color="secondary" onClick={onClickSignOut}>
             <LogoutIcon color="primary"/>
         </Button>
@@ -61,9 +43,6 @@ export const NavigationButtons = () => {
 
   return (
     <Box sx={{ ml: "auto" }}>
-      <IconButton sx={{ ml: 1, mr: 1, width: 42, minWidth: 36}} color="secondary" onClick={onClickToggleMode}>
-        {mode === 'dark' ? <DarkModeIcon fontSize="large" /> : <LightModeIcon fontSize="large" />}
-      </IconButton>
       <Button sx={{ width: 42, minWidth: 36}} variant="contained" component={Link} to={`/sign-in`} color="secondary">
           <LoginIcon color="primary"/>
       </Button>

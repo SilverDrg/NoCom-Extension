38f026f0b86ee9a44d0ff3280ecdd9db778e60bf
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-04 17:56:25 +0100

    Added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { Footer } from "./components/general/Footer";
 import { CommentNew } from "./components/comment/CommentNew";
 import { CommentNewButton } from "./components/comment/CommentNewButton";
 import { Comments } from "./components/comment/Comments";
+import { CommentDisplay } from "./components/comment/CommentDisplay";
 
 import { ForgotPassword } from "./components/user/ForgotPassword";
 import { Profile } from "./components/user/Profile";
@@ -31,7 +32,7 @@ const App: React.FC = () => {
   return (
     <HelmetProvider>
       <TokenContextProvider>
-        <div className="App">
+        <div className='App'>
           <ThemeContextProvider>
             <CssBaseline />
             <Box
@@ -44,19 +45,28 @@ const App: React.FC = () => {
               <Router>
                 <Navigation />
                 <Routes>
-                  <Route path="/" element={<Navigate replace to="/home" />} />
-                  <Route path="/home" element={<Home />} />
-                  <Route path="/about-us" element={<AboutUs />} />
+                  <Route
+                    path='/'
+                    element={<Navigate replace to='/comments' />}
+                  />
+                  <Route
+                    path='*'
+                    element={<Navigate replace to='/comments' />}
+                  />
 
-                  <Route path="/comments" element={<Comments />} />
-                  <Route path="/comment-new" element={<CommentNew />} />
+                  <Route path='/home' element={<Home />} />
+                  <Route path='/about-us' element={<AboutUs />} />
 
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/settings" element={<Settings />} />
+                  <Route path='/comments' element={<Comments />} />
+                  <Route path='/comments/:id' element={<CommentDisplay />} />
+                  <Route path='/comment-new' element={<CommentNew />} />
 
-                  <Route path="/sign-in" element={<SignIn />} />
-                  <Route path="/sign-up" element={<SignUp />} />
-                  <Route path="/forgot-pass" element={<ForgotPassword />} />
+                  <Route path='/profile' element={<Profile />} />
+                  <Route path='/settings' element={<Settings />} />
+
+                  <Route path='/sign-in' element={<SignIn />} />
+                  <Route path='/sign-up' element={<SignUp />} />
+                  <Route path='/forgot-pass' element={<ForgotPassword />} />
                 </Routes>
                 <Footer />
                 <CommentNewButton />

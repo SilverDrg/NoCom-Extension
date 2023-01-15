de9ec7a1614be7c4052595fd1e464554463d67ea
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-15 23:34:49 +0100

    Added handleLike functionality

--- src/components/comment/Comment.tsx
+++ src/components/comment/Comment.tsx
@@ -1,22 +1,17 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Grid,
-  Paper,
-  Typography,
-  Checkbox,
-  IconButton,
-  useTheme,
-} from "@mui/material";
-import { pink } from "@mui/material/colors";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Grid, Paper, Typography, Checkbox, IconButton, useTheme } from '@mui/material';
+import { pink } from '@mui/material/colors';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
-import { ColorModeContext } from "../session/ThemeContextProvider";
-import { CommentModel } from "../../models/Comment";
-import { useLoggedIn } from "../../hooks/useLoggedIn";
+import { ColorModeContext } from '../session/ThemeContextProvider';
+import { TokenContext } from '../session/TokenContextProvider';
+import { CommentModel } from '../../models/Comment';
+import { useLoggedIn } from '../../hooks/useLoggedIn';
+import { apiSetLike } from '../../util/apiCalls';
 
 export const Comment = (props: any) => {
   const comment: CommentModel = props.comment;
@@ -24,12 +19,18 @@ export const Comment = (props: any) => {
   const navigate = useNavigate();
   const [isLoggedIn] = useLoggedIn();
   const { mode } = React.useContext(ColorModeContext);
+  const { token } = React.useContext(TokenContext);
   const [Like, setLike] = React.useState(false);
+  const [LikesCount, setLikesCount] = React.useState(comment.likes);
   let isNSFW, isCommentOwner;
 
   const handleLike = () => {
     if (!isLoggedIn) return;
-    setLike((like) => !like);
+    apiSetLike(token, comment.id, !Like).then(response => {
+      console.log(response.data);
+      if (typeof response.data === 'number') setLikesCount(response.data);
+    });
+    setLike(like => !like);
   };
 
   const handleViewReplies = () => {
@@ -39,7 +40,7 @@ export const Comment = (props: any) => {
   if (comment.nsfw) {
     isNSFW = (
       <Grid item xs={3} md={3}>
-        <Typography variant='button' color='red'>
+        <Typography variant="button" color="red" sx={{ p: 1, display: 'inline-block' }}>
           NSFW
         </Typography>
       </Grid>
@@ -49,18 +50,18 @@ export const Comment = (props: any) => {
   if (comment.isOwner) {
     isCommentOwner = (
       <Grid item xs>
-        <Grid container direction='row-reverse'>
+        <Grid container direction="row-reverse">
           <Grid item>
             <IconButton
-              color='error'
-              aria-label='commentOwner'
-              component='label'
+              color="error"
+              aria-label="commentOwner"
+              component="label"
               onClick={handleViewReplies}
               sx={{ mr: 0.5 }}
             >
               <DeleteOutlineOutlinedIcon
                 sx={{
-                  "&.MuiSvgIcon-root": { fontSize: 18 },
+                  '&.MuiSvgIcon-root': { fontSize: 18 },
                 }}
               />
             </IconButton>
@@ -73,43 +74,36 @@ export const Comment = (props: any) => {
   return (
     <Paper key={comment.id} elevation={3} sx={{ m: 2, p: 0.5 }}>
       <Typography
-        variant='body1'
-        align='left'
+        variant="body1"
+        align="left"
         sx={{
           m: 1,
           borderBottom: 1,
-          borderColor:
-            mode === "light"
-              ? theme.palette.primary.main
-              : theme.palette.secondary.light,
+          borderColor: mode === 'light' ? theme.palette.primary.main : theme.palette.secondary.light,
         }}
       >
         {comment.username}
       </Typography>
-      <Typography variant='body2' align='left' sx={{ m: 1 }}>
+      <Typography variant="body2" align="left" sx={{ m: 1 }}>
         {comment.content}
       </Typography>
       <Grid sx={{ flexGrow: 1 }} container spacing={2}>
         <Grid item xs={12}>
-          <Grid container justifyContent='start' spacing={2}>
+          <Grid container justifyContent="start" spacing={2}>
             <Grid item>
               <IconButton
-                color={mode === "dark" ? "secondary" : "default"}
-                aria-label='replies'
-                component='label'
+                color={mode === 'dark' ? 'secondary' : 'default'}
+                aria-label="replies"
+                component="label"
                 onClick={handleViewReplies}
               >
                 <ChatBubbleOutlineIcon
                   sx={{
-                    "&.MuiSvgIcon-root": { fontSize: 18 },
+                    '&.MuiSvgIcon-root': { fontSize: 18 },
                   }}
                 />
               </IconButton>
-              <Typography
-                variant='body2'
-                align='left'
-                sx={{ p: 1, pl: 0, display: "inline-block" }}
-              >
+              <Typography variant="body2" align="left" sx={{ p: 1, pl: 0, display: 'inline-block' }}>
                 {comment.repliesCount}
               </Typography>
             </Grid>
@@ -117,24 +111,24 @@ export const Comment = (props: any) => {
               <Checkbox
                 icon={<FavoriteBorderIcon />}
                 checkedIcon={<FavoriteIcon />}
-                value='like'
+                value="like"
                 checked={Like}
                 onChange={handleLike}
                 sx={{
-                  color: "primary",
-                  "&.Mui-checked": {
+                  color: 'primary',
+                  '&.Mui-checked': {
                     color: pink[600],
                   },
-                  "& .MuiSvgIcon-root": { fontSize: 18 },
+                  '& .MuiSvgIcon-root': { fontSize: 18 },
                 }}
               />
               <Typography
-                variant='body2'
-                align='left'
-                sx={{ p: 1, pl: 0, display: "inline-block" }}
-                color={Like ? pink[500] : ""}
+                variant="body2"
+                align="left"
+                sx={{ p: 1, pl: 0, display: 'inline-block' }}
+                color={Like ? pink[500] : ''}
               >
-                {comment.likes}
+                {LikesCount}
               </Typography>
             </Grid>
             {isNSFW}
--- src/components/comment/CommentDisplay.tsx
+++ src/components/comment/CommentDisplay.tsx
@@ -1,46 +1,49 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import {
-  Box,
-  Grid,
-  Paper,
-  Typography,
-  Checkbox,
-  IconButton,
-  useTheme,
-} from "@mui/material";
-import { pink } from "@mui/material/colors";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
-import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, Grid, Paper, Typography, Checkbox, IconButton, useTheme } from '@mui/material';
+import { pink } from '@mui/material/colors';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 
-import { ColorModeContext } from "../session/ThemeContextProvider";
-import { CommentModel } from "../../models/Comment";
+import { ColorModeContext } from '../session/ThemeContextProvider';
+import { TokenContext } from '../session/TokenContextProvider';
+import { CommentModel } from '../../models/Comment';
+import { useLoggedIn } from '../../hooks/useLoggedIn';
+import { apiSetLike } from '../../util/apiCalls';
 
 export const CommentDisplay = () => {
   const comment: CommentModel = {
     id: 1,
-    username: "user",
+    username: 'user',
     likes: 1,
-    content: "Hello random content",
+    content: 'Hello random content',
     nsfw: true,
     replyTo: null,
-    createdAt: "today",
-    updatedAt: "today",
-    encryptedUrl: "",
+    createdAt: 'today',
+    updatedAt: 'today',
+    encryptedUrl: '',
     isOwner: true,
     repliesCount: 0,
     replies: [],
   };
   const navigate = useNavigate();
   const theme = useTheme();
+  const [isLoggedIn] = useLoggedIn();
   const { mode } = React.useContext(ColorModeContext);
-  const [Like, setLike] = React.useState(false);
+  const { token } = React.useContext(TokenContext);
+  const [like, setLike] = React.useState(false);
+  const [likesCount, setLikesCount] = React.useState(comment.likes);
   let isNSFW, isCommentOwner;
 
   const handleLike = () => {
-    setLike((like) => !like);
+    if (!isLoggedIn) return;
+    apiSetLike(token, comment.id, !like).then(response => {
+      console.log(response.data);
+      if (typeof response.data === 'number') setLikesCount(response.data);
+    });
+    setLike(like => !like);
   };
 
   const handleViewReplies = () => {
@@ -50,11 +53,7 @@ export const CommentDisplay = () => {
   if (comment.nsfw) {
     isNSFW = (
       <Grid item>
-        <Typography
-          variant='button'
-          color='red'
-          sx={{ p: 1, display: "inline-block" }}
-        >
+        <Typography variant="button" color="red" sx={{ p: 1, display: 'inline-block' }}>
           NSFW
         </Typography>
       </Grid>
@@ -64,18 +63,18 @@ export const CommentDisplay = () => {
   if (comment.isOwner) {
     isCommentOwner = (
       <Grid item xs>
-        <Grid container direction='row-reverse'>
+        <Grid container direction="row-reverse">
           <Grid item>
             <IconButton
-              color='error'
-              aria-label='commentOwner'
-              component='label'
+              color="error"
+              aria-label="commentOwner"
+              component="label"
               onClick={handleViewReplies}
               sx={{ mr: 0.5 }}
             >
               <DeleteOutlineOutlinedIcon
                 sx={{
-                  "&.MuiSvgIcon-root": { fontSize: 18 },
+                  '&.MuiSvgIcon-root': { fontSize: 18 },
                 }}
               />
             </IconButton>
@@ -89,43 +88,31 @@ export const CommentDisplay = () => {
     <Box>
       <Paper key={comment.id} elevation={3} sx={{ m: 2, p: 0.5 }}>
         <Typography
-          variant='body1'
-          align='left'
+          variant="body1"
+          align="left"
           sx={{
             m: 1,
             borderBottom: 1,
-            borderColor:
-              mode === "light"
-                ? theme.palette.primary.main
-                : theme.palette.secondary.light,
+            borderColor: mode === 'light' ? theme.palette.primary.main : theme.palette.secondary.light,
           }}
         >
           {comment.username}
         </Typography>
-        <Typography variant='body2' align='left' sx={{ m: 1 }}>
+        <Typography variant="body2" align="left" sx={{ m: 1 }}>
           {comment.content}
         </Typography>
         <Grid sx={{ flexGrow: 1 }} container spacing={2}>
           <Grid item xs={12}>
-            <Grid container justifyContent='start' spacing={2}>
+            <Grid container justifyContent="start" spacing={2}>
               <Grid item>
-                <IconButton
-                  color='secondary'
-                  aria-label='replies'
-                  component='label'
-                  onClick={handleViewReplies}
-                >
+                <IconButton color="secondary" aria-label="replies" component="label" onClick={handleViewReplies}>
                   <ChatBubbleOutlineIcon
                     sx={{
-                      "&.MuiSvgIcon-root": { fontSize: 18 },
+                      '&.MuiSvgIcon-root': { fontSize: 18 },
                     }}
                   />
                 </IconButton>
-                <Typography
-                  variant='body2'
-                  align='left'
-                  sx={{ p: 1, pl: 0, display: "inline-block" }}
-                >
+                <Typography variant="body2" align="left" sx={{ p: 1, pl: 0, display: 'inline-block' }}>
                   {comment.repliesCount}
                 </Typography>
               </Grid>
@@ -133,24 +120,24 @@ export const CommentDisplay = () => {
                 <Checkbox
                   icon={<FavoriteBorderIcon />}
                   checkedIcon={<FavoriteIcon />}
-                  value='like'
-                  checked={Like}
+                  value="like"
+                  checked={like}
                   onChange={handleLike}
                   sx={{
-                    color: "primary",
-                    "&.Mui-checked": {
+                    color: 'primary',
+                    '&.Mui-checked': {
                       color: pink[600],
                     },
-                    "& .MuiSvgIcon-root": { fontSize: 18 },
+                    '& .MuiSvgIcon-root': { fontSize: 18 },
                   }}
                 />
                 <Typography
-                  variant='body2'
-                  align='left'
-                  sx={{ p: 1, pl: 0, display: "inline-block" }}
-                  color={Like ? pink[500] : ""}
+                  variant="body2"
+                  align="left"
+                  sx={{ p: 1, pl: 0, display: 'inline-block' }}
+                  color={like ? pink[500] : ''}
                 >
-                  {comment.likes}
+                  {likesCount}
                 </Typography>
               </Grid>
               {isNSFW}

99844a2796eca158fc482ac03c778ee4e25e630d
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-15 23:34:18 +0100

    Added private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,63 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-import { CssBaseline, Box } from "@mui/material";
-import TokenContextProvider from "./components/session/TokenContextProvider";
-import ThemeContextProvider from "./components/session/ThemeContextProvider";
-import { Title } from "./components/general/Title";
-import { Home } from "./components/general/Home";
-import { AboutUs } from "./components/general/AboutUs";
-import { Navigation } from "./components/general/Navigation";
-import { Footer } from "./components/general/Footer";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { CssBaseline, Box } from '@mui/material';
+import TokenContextProvider from './components/session/TokenContextProvider';
+import ThemeContextProvider from './components/session/ThemeContextProvider';
+import { Title } from './components/general/Title';
+import { Home } from './components/general/Home';
+import { Navigation } from './components/general/Navigation';
+import { Footer } from './components/general/Footer';
+import { PrivateRoute } from './components/general/PrivateRoute';
 
-import { CommentNew } from "./components/comment/CommentNew";
-import { CommentNewButton } from "./components/comment/CommentNewButton";
-import { Comments } from "./components/comment/Comments";
-import { CommentDisplay } from "./components/comment/CommentDisplay";
+import { CommentNew } from './components/comment/CommentNew';
+import { CommentNewButton } from './components/comment/CommentNewButton';
+import { Comments } from './components/comment/Comments';
+import { CommentDisplay } from './components/comment/CommentDisplay';
 
-import { ForgotPassword } from "./components/user/ForgotPassword";
-import { Profile } from "./components/user/Profile";
-import { Settings } from "./components/user/Settings";
-import { SignIn } from "./components/user/SignIn";
-import { SignUp } from "./components/user/SignUp";
+import { ForgotPassword } from './components/user/ForgotPassword';
+import { Profile } from './components/user/Profile';
+import { Settings } from './components/user/Settings';
+import { SignIn } from './components/user/SignIn';
+import { SignUp } from './components/user/SignUp';
 
-import "./App.css";
+import './App.css';
 
 const App: React.FC = () => {
   return (
     <HelmetProvider>
       <TokenContextProvider>
-        <div className='App'>
+        <div className="App">
           <ThemeContextProvider>
             <CssBaseline />
             <Box
               sx={{
-                backgroundColor: "background.default",
-                color: "text.primary",
+                backgroundColor: 'background.default',
+                color: 'text.primary',
               }}
             >
               <Title />
               <Router>
                 <Navigation />
                 <Routes>
-                  <Route
-                    path='/'
-                    element={<Navigate replace to='/comments' />}
-                  />
-                  <Route
-                    path='*'
-                    element={<Navigate replace to='/comments' />}
-                  />
+                  <Route path="/" element={<Navigate replace to="/comments" />} />
+                  <Route path="*" element={<Navigate replace to="/comments" />} />
 
-                  <Route path='/home' element={<Home />} />
-                  <Route path='/about-us' element={<AboutUs />} />
+                  <Route path="/home" element={<Home />} />
 
-                  <Route path='/comments' element={<Comments />} />
-                  <Route path='/comments/:id' element={<CommentDisplay />} />
-                  <Route path='/comment-new' element={<CommentNew />} />
+                  <Route path="/comments" element={<Comments />} />
+                  <Route path="/comments/:id" element={<CommentDisplay />} />
 
-                  <Route path='/profile' element={<Profile />} />
-                  <Route path='/settings' element={<Settings />} />
+                  <Route element={<PrivateRoute />}>
+                    <Route path="/comment-new" element={<CommentNew />} />
 
-                  <Route path='/sign-in' element={<SignIn />} />
-                  <Route path='/sign-up' element={<SignUp />} />
-                  <Route path='/forgot-pass' element={<ForgotPassword />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/settings" element={<Settings />} />
+                  </Route>
+
+                  <Route path="/sign-in" element={<SignIn />} />
+                  <Route path="/sign-up" element={<SignUp />} />
+                  <Route path="/forgot-pass" element={<ForgotPassword />} />
                 </Routes>
                 <Footer />
                 <CommentNewButton />
---
+++ src/components/general/PrivateRoute.tsx
@@ -0,0 +1,7 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useLoggedIn } from '../../hooks/useLoggedIn';
+
+export const PrivateRoute = () => {
+  const [isLoggedIn] = useLoggedIn();
+  return isLoggedIn ? <Outlet /> : <Navigate to="/sign-in" replace />;
+};

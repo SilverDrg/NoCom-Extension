b1ec601981438da4b65a1b60e1548eb88d8cffce
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-01-15 23:36:54 +0100

    Updated useLoggedIn hook

--- src/hooks/useLoggedIn.tsx
+++ src/hooks/useLoggedIn.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import { TokenContext } from '../components/session/TokenContextProvider';
 
 export const useLoggedIn = (): [boolean] => {
-    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-    const { token } = React.useContext(TokenContext);
-
-    React.useEffect(() => {
-        if (!token) return;
-        setIsLoggedIn(true);
-    }, [setIsLoggedIn, token]);
-
-    return [isLoggedIn];
-}
+  const { token } = React.useContext(TokenContext);
+  return [!!token];
+};

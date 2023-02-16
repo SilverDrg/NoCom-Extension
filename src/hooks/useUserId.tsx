4be9e034b838f578a407aee72a302e70c710b769
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-02-16 16:46:31 +0100

    Created hook for fetching userId

---
+++ src/hooks/useUserId.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { TokenContext } from '../components/session/TokenContextProvider';
+
+export const useUserId = (): [string | null] => {
+  const { token } = React.useContext(TokenContext);
+  const [userId, setUserId] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    if (!token) setUserId(null);
+    const userId = localStorage.getItem('userId');
+    console.log('user id: ', userId);
+    if (!userId && userId !== '') setUserId(null);
+    else setUserId(userId);
+  }, [token]);
+
+  return [userId];
+};

d7b21f7c18f49c0cc81b8d44c129c171d0d59d1b
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-02-20 21:08:50 +0100

    Updated comment posting to support replies

--- src/components/comment/CommentNew.tsx
+++ src/components/comment/CommentNew.tsx
@@ -1,29 +1,32 @@
 import * as React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { Avatar, Button, TextField, Grid, Box, Typography, Container, FormControlLabel, Checkbox } from '@mui/material';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import {
+  Avatar,
+  Button,
+  TextField,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  FormControlLabel,
+  Checkbox,
+  useTheme,
+} from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
-import { TokenContext } from '../session/TokenContextProvider';
 
+import { TokenContext } from '../session/TokenContextProvider';
+import { ColorModeContext } from '../session/ThemeContextProvider';
 import { apiPostComment } from '../../util/apiCalls';
+import { useWebsiteUrl } from '../../hooks/useWebsiteUrl';
 
 export const CommentNew = () => {
+  const { mode } = React.useContext(ColorModeContext);
   const { token } = React.useContext(TokenContext);
   const [nsfw, setNsfw] = React.useState(false);
-  const [website, setWebsite] = React.useState<string | undefined>();
+  const [website, updateWebsite] = useWebsiteUrl();
   const navigate = useNavigate();
-
-  React.useEffect(() => {
-    chrome.tabs &&
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        tabs => {
-          console.log(tabs[0].url);
-        },
-      );
-  }, []);
+  const params = useParams();
+  const theme = useTheme();
 
   const handleNsfw = () => {
     setNsfw(!nsfw);
@@ -31,22 +34,23 @@ export const CommentNew = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    chrome.tabs &&
-      chrome.tabs.query(
-        {
-          active: true,
-          currentWindow: true,
-        },
-        tabs => {
-          setWebsite(tabs[0].url);
-        },
-      );
+    updateWebsite();
     const data = new FormData(event.currentTarget);
+    if (!data.get('comment') || data.get('comment') === '') return;
+    const newComment = {
+      content: data.get('comment') as string,
+      nsfw: nsfw,
+      website: website ?? '',
+      userId: localStorage.getItem('userId'),
+      replyId: params.id,
+    };
+    console.log('new comment: ', newComment);
     apiPostComment(token, {
       content: data.get('comment') as string,
       nsfw: nsfw,
       website: website ?? '',
       userId: localStorage.getItem('userId'),
+      replyId: params.id,
     }).then(response => {
       console.log(response);
     });
@@ -80,20 +84,38 @@ export const CommentNew = () => {
             id="comment"
             label="New comment"
             name="comment"
-            autoComplete="comment"
             autoFocus
           />
           <FormControlLabel
-            control={<Checkbox value="nsfw" checked={nsfw} onChange={handleNsfw} color="primary" />}
+            control={
+              <Checkbox
+                value="nsfw"
+                checked={nsfw}
+                onChange={handleNsfw}
+                color="primary"
+                sx={{
+                  color: 'secondary',
+                  '&.Mui-checked': {
+                    color: mode === 'light' ? theme.palette.primary.main : theme.palette.secondary.light,
+                  },
+                }}
+              />
+            }
             label="Nsfw"
           />
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color={mode === 'light' ? 'primary' : 'secondary'}
+            sx={{ mt: 3, mb: 2 }}
+          >
             Post
           </Button>
           <Grid container>
             <Grid item>
               <Link to="/" onClick={() => navigate(-1)} style={{ textDecoration: 'none' }}>
-                <Typography color="primary.dark" variant="body2">
+                <Typography color={mode === 'light' ? 'primary' : 'secondary.light'} variant="body2">
                   {'Back'}
                 </Typography>
               </Link>

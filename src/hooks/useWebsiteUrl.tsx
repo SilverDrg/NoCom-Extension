44756df7affe5546d2a18d346f4d5a2174707b10
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-02-24 02:34:28 +0100

    Added url hashing with crypto-hash

--- src/hooks/useWebsiteUrl.tsx
+++ src/hooks/useWebsiteUrl.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import z from 'zod';
+import { sha256 } from 'crypto-hash';
 
 const websiteUrl = z.string().url();
 
 export const useWebsiteUrl = (): [string, () => void] => {
   const [website, setWebsite] = React.useState<string | undefined>();
+
+  const hashUrl = React.useCallback(async (url: string) => {
+    return await sha256(url);
+  }, []);
 
   React.useEffect(() => {
     chrome.tabs &&
@@ -14,12 +19,13 @@ export const useWebsiteUrl = (): [string, () => void] => {
           currentWindow: true,
         },
         tabs => {
+          console.log('websitte hash: ', hashUrl(tabs[0].url ?? 'something else with pawbies'));
           console.log(tabs[0].url);
           const tab = tabs[0];
-          setWebsite(tab.url);
+          hashUrl(tab.url ?? 'undefined').then(hash => setWebsite(hash));
         },
       );
-  }, []);
+  }, [hashUrl]);
 
   const updateWebsite = React.useCallback(() => {
     chrome.tabs &&
@@ -31,10 +37,10 @@ export const useWebsiteUrl = (): [string, () => void] => {
         tabs => {
           console.log(tabs[0].url);
           const tab = tabs[0];
-          setWebsite(tab.url);
+          hashUrl(tab.url ?? 'undefined').then(hash => setWebsite(hash));
         },
       );
-  }, []);
+  }, [hashUrl]);
 
   const results = websiteUrl.safeParse(website);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion

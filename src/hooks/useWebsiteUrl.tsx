c83e51b24cecb3f681a67fc963371d603b71aeda
SilverDerg <55854114+SilverDerg@users.noreply.github.com>
2023-04-03 01:51:18 +0200

    Removed console logs

--- src/hooks/useWebsiteUrl.tsx
+++ src/hooks/useWebsiteUrl.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import z from 'zod';
 import { sha256 } from 'crypto-hash';
-
-const websiteUrl = z.string().url();
 
 export const useWebsiteUrl = (): [string, () => void] => {
   const [website, setWebsite] = React.useState<string | undefined>();
@@ -19,10 +16,10 @@ export const useWebsiteUrl = (): [string, () => void] => {
           currentWindow: true,
         },
         tabs => {
-          console.log('websitte hash: ', hashUrl(tabs[0].url ?? 'something else with pawbies'));
-          console.log(tabs[0].url);
           const tab = tabs[0];
-          hashUrl(tab.url ?? 'undefined').then(hash => setWebsite(hash));
+          hashUrl(tab.url ?? 'undefined').then(hash => {
+            setWebsite(hash);
+          });
         },
       );
   }, [hashUrl]);
@@ -35,15 +32,14 @@ export const useWebsiteUrl = (): [string, () => void] => {
           currentWindow: true,
         },
         tabs => {
-          console.log(tabs[0].url);
           const tab = tabs[0];
-          hashUrl(tab.url ?? 'undefined').then(hash => setWebsite(hash));
+          hashUrl(tab.url ?? 'undefined').then(hash => {
+            setWebsite(hash);
+          });
         },
       );
   }, [hashUrl]);
 
-  const results = websiteUrl.safeParse(website);
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  if (results.success) return [website!, updateWebsite];
-  else return ['undefined', updateWebsite];
+  return [website!, updateWebsite];
 };
